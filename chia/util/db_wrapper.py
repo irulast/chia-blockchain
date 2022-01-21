090544bd77afe6acc3c237cf165a1632d559a796
Nathan Joshi <nvjoshi@Nathans-MacBook-Pro.local>
2022-01-21 08:54:49 -0600

    removed unused dbwrapper transaction functions

--- chia/util/db_wrapper.py
+++ chia/util/db_wrapper.py
@@ -18,14 +18,3 @@ class DBWrapper:
         self.allow_upgrades = allow_upgrades
         self.lock = asyncio.Lock()
         self.db_version = db_version
-
-    async def begin_transaction(self):
-        await self.db.execute("BEGIN TRANSACTION")
-
-    async def rollback_transaction(self):
-        # Also rolls back the coin store, since both stores must be updated at once
-        if self.db.in_transaction:
-            await self.db.execute("ROLLBACK")
-
-    async def commit_transaction(self):
-        await self.db.commit()
