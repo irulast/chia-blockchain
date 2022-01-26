422ba559fd085cbda801c6289d45c0b756e59663
Nathan Joshi <nvjoshi@Nathans-MacBook-Pro.local>
2022-01-26 16:01:50 -0600

    add pg imports, env var logging

--- chia/util/db_factory.py
+++ chia/util/db_factory.py
@@ -1,10 +1,11 @@
 import os
-
+import logging
 from databases import Database
-
+log = logging.getLogger(__name__)
 
 def create_database(default_db_path: str) -> Database:
     if os.environ.get("CHIA_DB_CONNECTION", None) is not None:
+        log.error('using env variable: ' + os.environ.get("CHIA_DB_CONNECTION"))
         return Database(os.environ.get("CHIA_DB_CONNECTION"))
     else:
         return Database("sqlite:///{}".format(default_db_path))
--- chia/util/temp_file_db.py
+++ chia/util/temp_file_db.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 from chia.util.db_factory import create_database
 import tempfile
+import logging
+log = logging.getLogger(__name__)
 
 class TempFileDatabase:
     def __init__(self):
+        log.error('temp file db used')
         self.db_path = Path(tempfile.NamedTemporaryFile().name)
         if self.db_path.exists():
             self.db_path.unlink()
